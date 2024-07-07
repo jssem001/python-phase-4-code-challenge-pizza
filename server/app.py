62e277da14104c75705ffefa5185149904579f7e
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-07-08 01:07:24 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response,jsonify
 from flask_restful import Api, Resource
 import os
+import sys
+ 
+sys.setrecursionlimit(10**6)
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -20,9 +24,72 @@ db.init_app(app)
 api = Api(app)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route('/restaurants')
+def restaurants():
+    restaurants = []
+    for restaurant in Restaurant.query.all():
+        restaurant_dict = restaurant.to_dict()
+        restaurant.append(restaurant_dict)
+
+    response = make_response(
+        restaurants,
+        200
+    )
+
+    return response
+
+@app.route('/restaurants/<int:id>', methods=['GET','DELETE'])
+def restaurants_id(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    if request.method=='GET':
+        restaurant_dict = restaurant.to_dict()
+
+        response = make_response(
+            restaurant_dict,
+            200
+        )
+    elif request.method=='DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        response = make_response(
+            'deleted successfully',
+            204
+        )
+
+    return response
+
+@app.route('/pizzas', methods=['GET','POST'])
+def pizzas():
+    if request.method=='GET':
+        pizzas = []
+        for pizza in Pizza.query.all():
+            pizza_dict = pizza.to_dict()
+            pizzas.append(pizza_dict)
+
+        response = make_response(
+            pizzas,
+            200
+        )
+
+    elif request.method=='POST':
+        pizza=Pizza(
+            name=request.form['name'],
+            ingredients=request.form['ingredients']
+        )
+
+        db.session.add(pizza)
+        db.session.commit()
+
+        response = make_response(
+            pizza.to_dict(),
+            201
+        )
+
+    return response
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -21,8 +21,11 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    RestaurantPizza = db.relationship('RestaurantPizza', backref='restaurant')
+
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +39,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    RestaurantPizza = db.relationship('RestaurantPizza', backref='pizza')
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,10 +55,21 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    
 
     # add serialization rules
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas")
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError('Price must be between 1 and 30')
+        return price
+
+    
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
